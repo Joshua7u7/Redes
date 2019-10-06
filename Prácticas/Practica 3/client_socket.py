6097bb090f1a34813d6852b51d48bb2352d0c432
Josue Ruiz Hernandez <josue.ruiz@netlogistik.local>
2019-10-06 17:05:07 -0500

    listening

--- Prácticas/Practica 3/client_socket.py
+++ Prácticas/Practica 3/client_socket.py
@@ -11,3 +11,15 @@ class ClientSocket:
 
     def make_connection(self):
         self.client_socket.connect((self.host, self.port))
+
+    
+    def listen_to_server(self):
+        print("Listening to changes")
+        while True:
+            try:
+                server_message = self.client_socket.recv(1024)
+                server_message = server_message.decode("utf-8")
+                print(server_message)
+            except Exception:
+                print("error")
+                break
