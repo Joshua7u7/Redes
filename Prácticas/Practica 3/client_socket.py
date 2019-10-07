bd6371565d11ca38e71221c77123253b2d25af02
Josue Ruiz Hernandez <josue.ruiz@netlogistik.local>
2019-10-06 19:01:44 -0500

    actions on client

--- Prácticas/Practica 3/client_socket.py
+++ Prácticas/Practica 3/client_socket.py
@@ -1,7 +1,9 @@
 import socket 
 import traceback
+import os
 
 class ClientSocket:
+    path = "C:\\Users\\josue.ruiz\\Documents\\ESCOM\\Redes\\Prácticas\\Practica 3\\client_files\\" 
 
     def __init__(self, host, port):
         self.host = host
@@ -20,7 +22,22 @@ class ClientSocket:
             try:
                 server_message = self.client_socket.recv(1024)
                 server_message = server_message.decode("utf-8")
-                print(server_message)
+                self.make_action(server_message)
             except Exception:
                 traceback.print_exc()
                 break
+    
+    def make_action(self, server_message):
+        args = server_message.split(',')
+        args = [arg.strip() for arg in args]
+        if args[0] == 'created':
+            file = open(self.path+args[1], 'wb')
+            file.close()
+        elif args[0] == 'moved':
+            os.rename(self.path+args[1], self.path+args[2])
+        elif args[0] == 'deleted':
+            os.remove(self.path + args[1])
+        elif args[0] == 'modified':
+            file = open(self.path + args[1], 'wb')
+            file.write(args[2])
+            file.close()       
