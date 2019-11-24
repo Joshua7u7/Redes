4b3518b5249a07083bfdc6a541c24fad751a4c59
Josue Ruiz Hernandez <josue.ruiz@netlogistik.local>
2019-11-24 16:06:48 -0600

    deploy client

--- Proyecto/Client/netFiles/src/environments/environment.prod.ts
+++ Proyecto/Client/netFiles/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  API: 'https://c335caf4.ngrok.io'
 };
--- Proyecto/Client/netFiles/src/environments/environment.ts
+++ Proyecto/Client/netFiles/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API: 'http://localhost:3000'
+  API: 'https://c335caf4.ngrok.io'
 };
 
 /*
